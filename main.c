8a4c945fc747c1b064e354b368ccf2e127666792
Adel2411 <hadjarabadel.2411@gmail.com>
2023-12-27 21:16:55 +0100

    learning how to create a GTK button

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
 #include <gtk/gtk.h>
 
+
+void Hello_World(GtkWidget* window) {
+    printf("Hello Adel, you clicked!\n");
+}
+
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
+    GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 100);
+    // Create a button with the label "Click me"
+    GtkWidget* button = gtk_button_new_with_label("Click me");
+    /* Connect the 'clicked' signal to our callback function */
+    g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(Hello_World), NULL);
 
-    GtkWidget* window;
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    // Set the title of the window
-    gtk_widget_set_name(window, "First Window");
-    gtk_window_set_title(GTK_WINDOW(window), "My First GTK+ Window");
-    // Set the size of the window
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
+    //put the button in the window container
+    gtk_container_add(GTK_CONTAINER(window), button);
 
-    //close the gtk window
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
     //show the gtk window
     gtk_widget_show_all(window);
 
