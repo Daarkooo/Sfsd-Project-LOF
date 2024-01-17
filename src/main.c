ebc1e4f73f49e0c5b6fec6b429d6517836d56066
Adel2411 <hadjarabadel.2411@gmail.com>
2024-01-17 01:52:12 +0100

    SearchStudent test done

--- src/main.c
+++ src/main.c
@@ -10,93 +10,93 @@ int main(int argc, char *argv[])
     printf("Hello brother !, how are you today ?\n");
 
 
-    // int stop = 0;
-    // int option;
-    // char ch1[20], ch2[20];
-    // LOF_fileP fichierLOF;
-    // int blockNB, posNB, existe;
-    // do
-    // {
-    //     printf("\n\t---- MAIN MENU ----");
-    //     printf("\n1- Create a new LOF file");
-    //     printf("\n2- Edit an existing LOF file");
-    //     printf("\n3- EXIT the program");
-    //     printf("\n\nWhat option do you choose : ");
-    //     scanf("%d", &option);
-    //     switch (option)
-    //     {
-    //         case 1:
-    //             printf("\n\t---- Creating a new file ----");
-    //             printf("\nChoose a name for your file : ");
-    //             scanf("%s", ch1);
-    //             strcpy(ch2, ch1);
-    //             strcat(ch1, ".bin");
-    //             strcat(ch2, ".txt");
-    //             if (openLOF(fichierLOF, ch1, 'o') != NULL) {
-    //                 printf("\nCE FICHIER EXISTE DEJA, CHOISSISSEZ UN AUTRE NOM !\n");
-    //                 break;
-    //             }
-    //             int n;
-    //             printf("Combien d'etudiant voulez vous inserez : ");
-    //             scanf("%d",&n);
-    //             createLOF(fichierLOF, ch1, n);
-    //             extractLOF(fichierLOF, ch1, ch2);
-    //             printf("\nYour file was succesfully created, you can see the result inside the file : %s\n", ch2);
-    //             break;
-    //         case 2:
-    //             printf("\n\t---- Editing an old file ----");
-    //             printf("\nWhat's the name of your file : ");
-    //             scanf("%s", ch1);
-    //             strcat(ch1, ".bin");
-    //             if (openLOF(fichierLOF, ch1, 'o') == NULL)
-    //             {
-    //                 printf("\nYOUR FILE DOESN'T EXIST\n");
-    //                 break;
-    //             }
-    //             printf("\n1- Insert a student");
-    //             printf("\n2- Delete a student");
-    //             printf("\n3- Search for a student");
-    //             printf("\n4- Modify a student");
-    //             printf("\n5- Return to MAIN MENU <--");
-    //             printf("\n\nWhat option do you choose : ");
-    //             scanf("%d", &option);
-    //             switch (option)
-    //             {
-    //             case 1:
-    //                 /* code */
-    //                 break;
-    //             case 2:
+    int stop = 0;
+    int option;
+    char ch1[20], ch2[20];
+    LOF_fileP fichierLOF;
+    int blockNB, posNB, existe;
+    do
+    {
+        printf("\n\t---- MAIN MENU ----");
+        printf("\n1- Create a new LOF file");
+        printf("\n2- Edit an existing LOF file");
+        printf("\n3- EXIT the program");
+        printf("\n\nWhat option do you choose : ");
+        scanf("%d", &option);
+        switch (option)
+        {
+            case 1:
+                printf("\n\t---- Creating a new file ----");
+                printf("\nChoose a name for your file : ");
+                scanf("%s", ch1);
+                strcpy(ch2, ch1);
+                strcat(ch1, ".bin");
+                strcat(ch2, ".txt");
+                if (openLOF(fichierLOF, ch1, 'o') != NULL) {
+                    printf("\nCE FICHIER EXISTE DEJA, CHOISSISSEZ UN AUTRE NOM !\n");
+                    break;
+                }
+                int n;
+                printf("Combien d'etudiant voulez vous inserez : ");
+                scanf("%d",&n);
+                createLOF(fichierLOF, ch1, n);
+                extractLOF(fichierLOF, ch1, ch2);
+                printf("\nYour file was succesfully created, you can see the result inside the file : %s\n", ch2);
+                break;
+            case 2:
+                printf("\n\t---- Editing an old file ----");
+                printf("\nWhat's the name of your file : ");
+                scanf("%s", ch1);
+                strcat(ch1, ".bin");
+                if (openLOF(fichierLOF, ch1, 'o') == NULL)
+                {
+                    printf("\nYOUR FILE DOESN'T EXIST\n");
+                    break;
+                }
+                printf("\n1- Insert a student");
+                printf("\n2- Delete a student");
+                printf("\n3- Search for a student");
+                printf("\n4- Modify a student");
+                printf("\n5- Return to MAIN MENU <--");
+                printf("\n\nWhat option do you choose : ");
+                scanf("%d", &option);
+                switch (option)
+                {
+                case 1:
+                    /* code */
+                    break;
+                case 2:
 
-    //                 break;
-    //             case 3:
-    //                 printf("\nEntrez le matricule de l'etudiant que vous cherchez : ");
-    //                 scanf("%d",&n);
-    //                 SearchStudent(fichierLOF, "test.bin", n, &blockNB, &posNB, &existe);
-    //                 if (existe == 1)
-    //                     printf("\nETUDIANT TROUVE :\nBlock : %d\nPosition : %d\n", blockNB, posNB);
-    //                 else
-    //                     printf("\nCETTE ETUDIANT N'EXISTE PAS\n");
-    //                 break;
-    //             case 4:
+                    break;
+                case 3:
+                    printf("\nEntrez le matricule de l'etudiant que vous cherchez : ");
+                    scanf("%d",&n);
+                    SearchStudent(fichierLOF, ch1, n, &blockNB, &posNB, &existe);
+                    if (existe == 1)
+                        printf("\nETUDIANT TROUVE :\nBlock : %d\nPosition : %d\n", blockNB, posNB);
+                    else
+                        printf("\nCETTE ETUDIANT N'EXISTE PAS\n");
+                    break;
+                case 4:
 
-    //                 break;
-    //             case 5:
+                    break;
+                case 5:
 
-    //                 break;
+                    break;
                 
-    //             default:
-    //                 printf("\nYOU CHOOSED A WRONG OPTION !\n");
-    //                 break;
-    //             }
-    //             break;
-    //         case 3:
-    //             stop = 1;
-    //             break;
-    //         default:
-    //             printf("\nYOU CHOOSED A WRONG OPTION !\n");
-    //             break;
-    //     }
-    // } while (stop == 0);
+                default:
+                    printf("\nYOU CHOOSED A WRONG OPTION !\n");
+                    break;
+                }
+                break;
+            case 3:
+                stop = 1;
+                break;
+            default:
+                printf("\nYOU CHOOSED A WRONG OPTION !\n");
+                break;
+        }
+    } while (stop == 0);
     
 
 
@@ -238,24 +238,27 @@ int main(int argc, char *argv[])
 
     // closeLOF(fichierLOF);
 
+    //--------- SearchStudent test ---------
+    // LOF_fileP fichierLOF;
+    // int n;
+    // printf("Combien d'etudiant voulez vous inserez : ");
+    // scanf("%d",&n);
+
+    // int blockNB, posNB, existe;
+    // createLOF(fichierLOF, "test.bin", n);
+    // extractLOF(fichierLOF, "test.bin", "test.txt");
+    // printf("Quelle est la valeur que vous recherchez : ");
+    // scanf("%d",&n);
+    // SearchStudent(fichierLOF, "test.bin", n, &blockNB, &posNB, &existe);
+    // if (existe == 1)
+    //     printf("\nETUDIANT TROUVE :\nBlock : %d\nPosition : %d\n\n", blockNB, posNB);
+    // else
+    //     printf("\nCETTE ETUDIANT N'EXISTE PAS\n\n");
+
 
     //---------------- FONCTIONS PAR ENCORE VÉRIFIÉS ---------------
 
-    LOF_fileP fichierLOF;
-    int n;
-    printf("Combien d'etudiant voulez vous inserez : ");
-    scanf("%d",&n);
-
-    int blockNB, posNB, existe;
-    createLOF(fichierLOF, "test.bin", n);
-    extractLOF(fichierLOF, "test.bin", "test.txt");
-    printf("Quelle est la valeur que vous recherchez : ");
-    scanf("%d",&n);
-    SearchStudent(fichierLOF, "test.bin", n, &blockNB, &posNB, &existe);
-    if (existe == 1)
-        printf("\nETUDIANT TROUVE :\nBlock : %d\nPosition : %d\n\n", blockNB, posNB);
-    else
-        printf("\nCETTE ETUDIANT N'EXISTE PAS\n\n");
+    
 
     
 
