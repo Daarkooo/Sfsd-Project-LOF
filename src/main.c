c63a7a8a065446a64bc34919e26f38199b19f7bf
Daarkooo <hichemhpro@gmail.com>
2024-01-25 12:28:21 +0100

    final version ..

--- src/main.c
+++ src/main.c
@@ -60,71 +60,85 @@ int main(int argc, char *argv[])
                         printf("\nYOUR FILE DOESN'T EXIST\n");
                         break;
                     }
+
+                    edit_menu:
                     printf("\n1- Insert a student");
                     printf("\n2- Delete a student");
                     printf("\n3- Search for a student");
                     printf("\n4- Modify a student");
                     printf("\n5- Return to MAIN MENU <--");
                     printf("\n\nWhat option do you choose : ");
-                    scanf("%d", &option);
-                    switch (option)
-                    {
-                    case 1:
-                            // while(1){
-                            //     printf("Quelle est le matricule de l'etudiant que vous voulez l'inserer: ");
-                            //     scanf("%d",&n);  
-
-                            //     SearchInsertionPosition(fichierLOF, ch1, n,&blockNB, &posNB);
-                            //     printf("block nb = %d - pos = %d\n",blockNB,posNB);
-                            // }
-                            fichierLOF = openLOF(fichierLOF, ch1, 'o');
+                    if(scanf("%d", &option) == 1){
+                        switch (option)
+                        {
+                        case 1:
+                                
+                                fichierLOF = openLOF(fichierLOF, ch1, 'o');
                                 student = malloc(sizeof(Student)); 
                                 printf("create student\n");
                                 createStudent(student);
+                                // while (student->matricule != )
+                                // {
+                                //     /* code */
+                                // }
                                 
                                 insertStudent(fichierLOF, ch1, student);
-                
                                 extractLOF(fichierLOF, ch1, ch2);
                                 printHeader(fichierLOF, ch1);
-                            
-                        break;
-                    case 2:
-                            printf("\nEntrez le matricule de l'etudiant a supprimer : ");
-                            scanf("%d",&n);
-                            DeleteStudent(fichierLOF, ch1, n);
-                            extractLOF(fichierLOF, ch1, ch2);
-                        break;
-                    case 3:
-                        
-                        do
-                        {
-                            printf("Entrez le matricule de l'etudiant que vous cherchez : ");
-                            scanf("%d",&n);
-                            SearchStudent(fichierLOF, ch1, n, &blockNB, &posNB, &existe);
-                            if (existe == 1){
-                                printf("\nETUDIANT TROUVE :\nBlockID : %d\nPosition : %d\n", blockNB, posNB);
-                                break;}
-                            else{
-                                printf("\nCETTE ETUDIANT N'EXISTE PAS\n");
-                                printf("Vous vous etes trompes? (o/n): ");
-                                scanf(" %c",&trompe);
-                                }
-                        } while (trompe == 'o' || trompe == 'O');
-                    
-                        break;
-                    case 4:
+                                
+                            goto edit_menu;
+                        case 2:
+                                printf("\nEntrez le matricule de l'etudiant a supprimer : ");
+                                scanf("%d",&n);
+                                DeleteStudent(fichierLOF, ch1, n);
+                                extractLOF(fichierLOF, ch1, ch2);
 
-                        break;
-                    
-                    case 5:
-                        goto main_menu;
-                        break;
-                    
-                    default:
-                        printf("\nYOU CHOOSED A WRONG OPTION !\n");
-                        break;
+                                printHeader(fichierLOF, ch1);
+                            goto edit_menu;
+                        case 3:
+                            
+                            do
+                            {
+                                printf("Entrez le matricule de l'etudiant que vous cherchez : ");
+                                scanf("%d",&n);
+                                
+                                
+                                SearchStudent(fichierLOF, ch1, n, &blockNB, &posNB, &existe);
+                                if (existe == 1){
+                                    printf("\nETUDIANT TROUVE :\nBlockID : %d\nPosition : %d\n", blockNB, posNB);
+                                    break;}
+                                else{
+                                    printf("\nCETTE ETUDIANT N'EXISTE PAS\n");
+                                    printf("Vous vous etes trompes? (o/n): ");
+                                    scanf(" %c",&trompe);
+                                    }
+                            } while (trompe == 'o' || trompe == 'O');
+
+                            goto edit_menu;
+
+                        case 4:
+                            student = malloc(sizeof(Student)); 
+                            printf("edit student\n");
+                            createStudent(student);
+
+                            ModifyStudent(fichierLOF,ch1,student);
+                            extractLOF(fichierLOF, ch1, ch2);
+
+                            goto edit_menu;
+                        
+                        case 5:
+                            goto main_menu;
+                        
+                        default:
+                            printf("\nYOU CHOOSED A WRONG OPTION !\n");
+                            goto edit_menu;
+                        }
                     }
-                    break;
+                    else{
+                        printf("please enter a valid number!\n");
+                        while((getchar() != '\n'));
+                        goto edit_menu;
+                    }
                 case 3:
                     stop = 1;
                     break;
@@ -140,85 +154,7 @@ int main(int argc, char *argv[])
     } while (stop == 0);
     
 
-    // ----------Delete fun test ---------------------------------
-    // LOF_fileP fichierLOF= openLOF(fichierLOF,"hichem.bin",'o');
-    // printf("First Block: %d\n", fichierLOF->header->firstBlock);
-    // printf("Last Block: %d\n", fichierLOF->header->lastBlock);
-    // printf("Number of Blocks: %d\n", fichierLOF->header->nbBlocks);
-    // printf("Number of Students: %d\n", fichierLOF->header->nbStudents);
+    closeLOF(fichierLOF);
+    return 0;
 
-    // int n;
-    // printf("Quelle est le matricule de l'etudiant que vous voulez le supprimer : ");
-    // scanf("%d",&n);
-    // DeleteStudent(fichierLOF, "hichem.bin",n);
-    // extractLOF(fichierLOF, "hichem.bin", "hichem.txt");
-
-
-
-    // LOF_fileP fichierLOF;
-    // fichierLOF = openLOF(fichierLOF, "test.bin", 'o');
-    // // createLOF(fichierLOF, "test.bin",8);
-    // extractLOF(fichierLOF, "test.bin","test.txt");
-    // printHeader(fichierLOF,"test.bin");
-    // int n;
-    // printf("Quelle est le matricule de l'etudiant que vous voulez le supprimer : ");
-    // scanf("%d",&n);
-    // DeleteStudent(fichierLOF, "test.bin",n);
-    // extractLOF(fichierLOF, "test.bin", "test.txt");
-
-    
-    // ----------InsertSearchPosition fun test ---------------------------------
-    // int n;
-
-    // StudentP student;
-    //  LOF_fileP fichierLOF; 
-    // int blockNB, posNB;
-    // // createLOF(fichierLOF,"test.bin",3);
-    // fichierLOF = openLOF(fichierLOF,"test.bin",'o');
-    // printHeader(fichierLOF,"test.bin");
-    // while (1)
-    // {
-    //     printf("matricule : ");
-    //     scanf("%d",&n);
-    //     printf("searching..\n");
-    //     SearchInsertionPosition(fichierLOF,"test.bin",n, &blockNB, &posNB);
-    //     extractLOF(fichierLOF, "test.bin", "test.txt");
-
-    //     printf("block: %d, position: %d \n",blockNB,posNB);
-    // }
-    
-   
-    // ----------Insert function --------------------------------
-    //    LOF_fileP fichierLOF = openLOF(fichierLOF,"hichem.bin",'o');
-        // int n;
-        // LOF_fileP fichierLOF;
-
-        // StudentP student;
-
-        // // int blockNB, posNB;
-        // // // scanf("%d",&n);
-        // // // createLOF(fichierLOF, "test.bin", 5);
-        //  fichierLOF = openLOF(fichierLOF, "random.bin", 'o');
-        // // // extractLOF(fichierLOF, "test.bin", "test.txt");
-        // // // printHeader(fichierLOF,"hichem.bin");
-        // while(1){
-        // //     // printf("Quelle est le matricule de l'etudiant que vous voulez l'inserer: ");
-        // //     // scanf("%d",&n);  
-
-        // //     // DeleteStudent(fichierLOF,"test.bin",n);
-        // //     // extractLOF(fichierLOF, "test.bin", "test.txt");
-        //     printf("create student\n");
-        //     createStudent(student);
-        //     printStudent(student);
-        // //     // SearchInsertionPosition(fichierLOF, "test.bin", n,&blockNB, &posNB);
-        //     insertStudent(fichierLOF, "random.bin", student);
-        // //     // printf("block nb = %d - pos = %d\n",blockNB,posNB);
-        //     extractLOF(fichierLOF, "random.bin", "random.txt");
-        //     // printf("\nprinting header: \n");
-        //     // printHeader(fichierLOF,"test.bin");
-        // }
-
-
-        // closeLOF(fichierLOF);
-        return 0;
 }
